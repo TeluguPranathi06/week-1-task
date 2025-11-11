cf145fc48395e7ca6be2686dcdf8c35a7f0b7d31
TeluguPranathi06 <telugupranathi06@gmail.com>
2025-11-11 22:15:33 +0530

    initial commit

---
+++ basics.py
@@ -0,0 +1,9 @@
+#personal information manager
+Name = "telugu pranathi";
+Age = 20;
+city = "kurnool";
+hobbies = "watching movies, cooking, listening music";
+print(Name);
+print(Age);
+print(city);
+print(hobbies);
